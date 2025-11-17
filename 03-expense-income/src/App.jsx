9f0c8a6c93c9f3c83f0904aa1d9a630f7d839171
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-11-17 15:22:56 +0100

    handle input

--- 03-expense-income/src/App.jsx
+++ 03-expense-income/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-
   const [input, setInput] = useState({
     statement: "",
     amount: "",
     statementType: ""
+  })
+  const[showError, setShowError] = useState({
+    statement: false,
+    amount:false
   })
 
   const handleUpdateInput = (e) => {
@@ -15,6 +18,32 @@ function App() {
       ...input,
       [e.target.name]: e.target.value
     })
+  }
+
+  const handleAddNewStatement = () => {
+
+    console.log('handle statement');
+
+    const { statement, amount } = input;
+
+    if(!statement) {
+      return setShowError({
+        statement: true,
+        amount: false
+      })
+    }
+    else if(!amount) {
+      return setShowError({
+        statement: false,
+        amount: true
+      })
+    } 
+    else {
+      return setShowError({
+        statement: false,
+        amount: false
+      })
+    }
   }
 
   return (
@@ -27,12 +56,16 @@ function App() {
             value={input.statement}
             onChange={handleUpdateInput}
             name="statement"
+            style = {
+              showError.statement ? { borderColor: "rgb(206, 76,76)"} : null
+            }
             />
           <input 
             type="number" placeholder="$5000" 
             value={input.amount}
             onChange={handleUpdateInput} 
             name="amount"
+            style={showError.amount ? { borderColor: "rgb(206, 76,76)"} : null}
           />
           <select onChange={handleUpdateInput}
             value={input.statementType}
@@ -41,7 +74,7 @@ function App() {
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
-          <button>+</button>
+          <button onClick={()=>handleAddNewStatement()}>+</button>
         </div>
         <div>
           <div className="card">
