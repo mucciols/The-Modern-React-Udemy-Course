b3a721bb7683a0b325262c49276f01753e913563
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-11-17 14:04:02 +0100

    2 way bindings

--- 03-expense-income/src/App.jsx
+++ 03-expense-income/src/App.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [statement, setStatement]  = useState("");
+  const [amount, setAmount]  = useState("");
+  const [statementType, setStatementType]  = useState("income");
+
   return (
     <main>
       <div>
         <h1 className="total-text">0</h1>
         <div className="input-container">
-          <input type="text" placeholder="Income or expense" />
+          <input 
+            type="text" placeholder="Inc. or exp." 
+            value={statement}
+            onChange={(e)=> setStatement(e.target.value) } />
+          <input 
+            type="number" placeholder="$5000" 
+            value={amount}
+            onChange={(e)=> setAmount(e.target.value) } />
           <input type="number" />
-          <select>
+          <select onChange={(e)=>{
+            setStatement(e.target.value)
+          }}
+            value={statementType}
+          >
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
