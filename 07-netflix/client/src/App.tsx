85781aadcbe0726c205608aa9f3368791ab32395
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-11-27 16:25:35 +0100

    netflix banner

--- 07-netflix/client/src/App.tsx
+++ 07-netflix/client/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HomeBanner from './components/HomeBanner'
 import NavBar from './components/NavBar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className='bg-indigo-400'>
       <NavBar />
+      <HomeBanner />
     </div>
   )
 }
---
+++ 07-netflix/client/src/components/HomeBanner.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const HomeBanner = (props) => {
+  return (
+    <div className="h-screen w-screen relative">
+      <img className="w-full h-full" src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/a822ccf9-e1b7-4352-92c7-aa9cd34f8931/CA-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="" />
+    </div>
+  )
+};
+
+export default HomeBanner;
