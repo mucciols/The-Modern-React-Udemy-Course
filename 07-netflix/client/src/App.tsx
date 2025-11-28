39b42d1e448075bef6447e1166c4293e8b59e777
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-11-28 15:58:25 +0100

    pagina di login

--- 07-netflix/client/src/App.tsx
+++ 07-netflix/client/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 
 
 function App() {
 
   return (
     <div >
-      <HomePage />      
+      <LoginPage />    
     </div>
   )
 }
