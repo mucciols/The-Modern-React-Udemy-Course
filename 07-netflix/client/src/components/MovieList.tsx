5e7c73db438b1dbb2ca34a73abbc3bd1fdb01d90
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-01 19:40:28 +0100

    piccoli fix relativi al css

--- 07-netflix/client/src/components/MovieList.tsx
+++ 07-netflix/client/src/components/MovieList.tsx
@@ -6,8 +6,10 @@ export default function MovieList( { movies } : {movies:Movie[]}) {
     <div className="px-12 mt-4 space-y-8">
       <div>
         <p className="text-black text-2xl font-semibold mb-4">Popular Shows</p>
-        <div className="flex flwz-wrap gap-2">
-         {movies.map(movie => <MovieCard key={movie.id} movie={movie} /> )}
+        <div className="flex flex-wrap gap-2">
+         {movies.map(movie => ( 
+          <MovieCard key={movie.id} movie={movie} />
+          ))}
         </div>
       </div>
     </div>
