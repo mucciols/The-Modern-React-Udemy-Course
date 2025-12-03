df5738e99e2ea4eb193dd0791ab5145329c83464
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-03 21:09:57 +0100

    aggiornamenti

--- 07-netflix/server/routes/auth.js
+++ 07-netflix/server/routes/auth.js
@@ -1,6 +1,10 @@
 
 const { PrismaClient } = require("@prisma/client");
 const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const bcrypt = require("bcrypt")
+const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
+
 const adapter = new PrismaMariaDb({
   host:     process.env.MYSQL_HOST || 'localhost',
   port:     process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 3306,
@@ -14,15 +18,14 @@ const prisma = new PrismaClient({
   errorFormat: 'pretty',
   adapter: adapter
 })
-const router = require("express").Router();
-const { check, validationResult } = require("express-validator");
+
 
 router.post("/signup", [
     check("email", "Please input a valid email").isEmail(),
     check("password","Please input a password with a min length of 6").isLength({ min: 6 }),
     check("username", "Please input a username with minleght of 6").isLength({ min: 6 })
-], async(req, res)=>{
-  const errors = validationResult(req);
+], async (req, res) => {
+  const errors = validationResult(req, res);
 
   if(!errors.isEmpty()) {
     return res.status(400).json({
@@ -30,9 +33,11 @@ router.post("/signup", [
     })
   }
 
+  const{ email, password, username } = req.body;
+
   const user = await prisma.user.findUnique({
     where: {
-      email : req.body.email
+      email : email
     }
   })
   
@@ -41,12 +46,10 @@ router.post("/signup", [
       errors: [{msg: "this user already exists"}],
     })
   }
-
-
   
-  res.send("VALID");
-
-  const{ email, password, username } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  res.send(hashPassword);
+  
 });
 
 module.exports = router;
