4fb2350c41027de36bcc5aef72a2b962e742ee6a
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-03 20:23:25 +0100

    check user valid

--- 07-netflix/server/routes/auth.js
+++ 07-netflix/server/routes/auth.js
@@ -1,3 +1,19 @@
+
+const { PrismaClient } = require("@prisma/client");
+const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const adapter = new PrismaMariaDb({
+  host:     process.env.MYSQL_HOST || 'localhost',
+  port:     process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 3306,
+  user:     'root',
+  password: 'mu66:o::',
+  database: 'modern_react',
+});
+
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+  errorFormat: 'pretty',
+  adapter: adapter
+})
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
 
@@ -13,6 +29,20 @@ router.post("/signup", [
       errors: errors.array()
     })
   }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email : req.body.email
+    }
+  })
+  
+  if(user) {
+    return res.status(400).json({
+      errors: [{msg: "this user already exists"}],
+    })
+  }
+
+
   
   res.send("VALID");
 
