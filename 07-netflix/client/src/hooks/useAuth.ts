5e3124b3e09d8deb69cb018b050fc8e598cd1152
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-04 16:01:39 +0100

    cookie set token

--- 07-netflix/client/src/hooks/useAuth.ts
+++ 07-netflix/client/src/hooks/useAuth.ts
@@ -1,20 +1,28 @@
 import axios from 'axios'
+import Cookie from "universal-cookie"
+
+const cookie = new Cookie();
 
 const useAuth = () => {
 
     const login = async ({ email, password }:{email: string; password:string}) => {
       const response = await axios.post("http://localhost:8080/auth/login", { 
-        email, password
+        email, 
+        password
       })
-
+      const { token } = response.data;
+      cookie.set("session_cookie", token);
       return response.data;
     }
 
     const signup = async ({ email, password, username }:{email: string; password:string; username: string}) => {
       const response = await axios.post("http://localhost:8080/auth/signup", { 
-        email, password, username
+        email, 
+        password, 
+        username
       })
-
+      const { token } = response.data;
+      cookie.set("session_cookie", token);
       return response.data;
     }
 
