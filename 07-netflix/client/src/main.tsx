cac0ee719c19007d985a3987dc5c235532f8e593
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-04 16:33:10 +0100

    react redux

--- 07-netflix/client/src/main.tsx
+++ 07-netflix/client/src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from  'react-router-dom'
-import HomePage from './pages/HomePage.tsx'
-import LoginPage from './pages/LoginPage.tsx'
-import PlansPage from './pages/PlantsPage.tsx'
-import BrowsePage from './pages/BrowsePage.tsx'
-import WatchPage from './pages/WatchPage.tsx'
-import MovieWithPrisma from './pages/MovieWithPrisma.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from "react-redux";
+import { store } from "./app/store"
+import './index.css';
+import App from './App.tsx';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from  'react-router-dom';
+import HomePage from './pages/HomePage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+import PlansPage from './pages/PlantsPage.tsx';
+import BrowsePage from './pages/BrowsePage.tsx';
+import WatchPage from './pages/WatchPage.tsx';
+import MovieWithPrisma from './pages/MovieWithPrisma.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,5 +26,7 @@ const router = createBrowserRouter(
 ) 
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
 )
